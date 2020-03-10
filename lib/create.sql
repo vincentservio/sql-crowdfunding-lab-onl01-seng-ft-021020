313a26a741da69d0894e4c9bcbd77bc7e11098e5
Vincent Servio <vincentservio@gmail.com>
2020-03-10 19:15:46 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
+ 
+INSERT INTO groceries VALUES (1, "Apples", 2, 1);
+INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
+INSERT INTO groceries VALUES(3, "Peaches", 6, 3);
